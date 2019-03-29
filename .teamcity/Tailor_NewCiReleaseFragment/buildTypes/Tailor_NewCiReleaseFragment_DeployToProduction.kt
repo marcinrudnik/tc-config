494a7aa2123daa9a3ae7e9d1fc43753cfe837fba
PRACUJ\marcin.rudnik <marcin.rudnik@pracuj.pl>
2019-03-29 14:43:24 +0100

    project templates

--- .teamcity/Tailor_NewCiReleaseFragment/buildTypes/Tailor_NewCiReleaseFragment_DeployToProduction.kt
+++ .teamcity/Tailor_NewCiReleaseFragment/buildTypes/Tailor_NewCiReleaseFragment_DeployToProduction.kt
@@ -1,10 +1,9 @@
 package Tailor_NewCiReleaseFragment.buildTypes
 
-import _Self.buildTypes.PdDeployFrontProjectToOctopus
 import jetbrains.buildServer.configs.kotlin.v2018_2.*
 
 object Tailor_NewCiReleaseFragment_DeployToProduction : BuildType({
-    templates(PdDeployFrontProjectToOctopus)
+
     name = "Deploy to Production"
 
     buildNumberPattern = "${Tailor_NewCiReleaseFragment_ReleaseStage.depParamRefs.buildNumber}"
